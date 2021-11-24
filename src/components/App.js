97b89ac51e26d3156151219829ae90ef9a6a8ebd
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2021-11-24 19:34:59 +0100

    Add the Add product functionality in the frontend

--- src/components/App.js
+++ src/components/App.js
@@ -4,45 +4,48 @@ import logo from "../logo.png";
 import "./App.css";
 import Marketplace from "../abis/Marketplace.json";
 import Navbar from "./Navbar";
+import Main from "./Main";
 
 class App extends Component {
   async componentWillMount() {
     await this.loadWeb3();
-    console.log(window.web3);
     await this.loadBlockchainData();
   }
+
   async loadWeb3() {
-    // there is a good code here https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable(); // // Request account access
+      await window.ethereum.enable();
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
     } else {
       window.alert(
-        "Non-ethereum browser detected. You shoulder consider trying Metamask. thank you."
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
       );
     }
   }
+
   async loadBlockchainData() {
     const web3 = window.web3;
     // Load account
     const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
-    this.setState({
-      account: accounts[0],
-    });
+    this.setState({ account: accounts[0] });
     const networkId = await web3.eth.net.getId();
     const networkData = Marketplace.networks[networkId];
     if (networkData) {
-      const marketplace = new web3.eth.Contract(
+      const marketplace = web3.eth.Contract(
         Marketplace.abi,
-        networkData.address // the address of the contract.
+        networkData.address
       );
+      this.setState({ marketplace });
+      const productCount = await marketplace.methods.productCount().call();
+      console.log(productCount.toString());
+      this.setState({ loading: false });
     } else {
-      window.alert("Marketplace contract not deployed to detected netowrk.");
+      window.alert("Marketplace contract not deployed to detected network.");
     }
   }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -51,24 +54,34 @@ class App extends Component {
       products: [],
       loading: true,
     };
+
+    this.createProduct = this.createProduct.bind(this);
   }
+
+  createProduct(name, price) {
+    this.setState({ loading: true });
+    this.state.marketplace.methods
+      .createProduct(name, price)
+      .send({ from: this.state.account })
+      .once("receipt", (receipt) => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
-        <Navbar account={this.state.account}></Navbar>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="https://www.facebook.com/zied.brah75/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Zieds marketplace</h1>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              {this.state.loading ? (
+                <div id="loader" className="text-center">
+                  <p className="text-center">Loading...</p>
+                </div>
+              ) : (
+                <Main createProduct={this.createProduct} />
+              )}
             </main>
           </div>
         </div>
